533ac59c8ad25121d1ce4e60cfcd06c9c49ba51e
degex26 <workmolko@gmail.com>
2026-06-07 12:03:32 +0700

    Update 1_hq-cli.sh

--- 1_hq-cli.sh
+++ 1_hq-cli.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+apt-get update
+apt-get install -y chrony
+
 timedatectl set-timezone Asia/Novosibirsk
 hostnamectl set-hostname hq-cli.au-team.irpo
 
 echo 'NM_CONTROLLED=no' >> /etc/net/ifaces/ens18/options
 echo 'DISABLED=no' >> /etc/net/ifaces/ens18/options
 systemctl restart network
+
+echo "server 172.16.1.1 iburst" > /etc/chrony.conf
+systemctl restart chronyd

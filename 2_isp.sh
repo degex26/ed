517ad6868bdac7cf574812a036cf21f9ae9d0455
degex26 <workmolko@gmail.com>
2026-06-18 21:34:10 +0700

    Update 2_isp.sh

--- 2_isp.sh
+++ 2_isp.sh
@@ -14,7 +14,7 @@ server {
     auth_basic "Restricted Access";
     auth_basic_user_file /etc/nginx/.htpasswd;
     location / {
-        proxy_pass http://172.16.1.2:8080;
+        proxy_pass http://172.16.30.2:8080;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
     }
@@ -23,7 +23,7 @@ server {
     listen 80;
     server_name docker.au-team.irpo;
     location / {
-        proxy_pass http://172.16.2.2:8080;
+        proxy_pass http://172.16.40.2:8080;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
     }

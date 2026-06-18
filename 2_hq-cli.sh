0c794b910a55809b20c19b74ae1abbb33ae30d07
degex26 <workmolko@gmail.com>
2026-06-18 21:36:29 +0700

    Update 2_hq-cli.sh

--- 2_hq-cli.sh
+++ 2_hq-cli.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 mkdir -p /mnt/nfs
-mount 192.168.1.2:/raid/nfs /mnt/nfs
-echo "192.168.1.2:/raid/nfs /mnt/nfs nfs defaults,_netdev 0 0" >> /etc/fstab
+mount 192.168.113.2:/raid/nfs /mnt/nfs
+echo "192.168.113.2:/raid/nfs /mnt/nfs nfs defaults,_netdev 0 0" >> /etc/fstab
 
 echo "search au-team.irpo" > /etc/resolv.conf
-echo "nameserver 192.168.2.2" >> /etc/resolv.conf
-echo "nameserver 192.168.1.2" >> /etc/resolv.conf
+echo "nameserver 192.168.113.2" >> /etc/resolv.conf
+
+# Яндекс Браузер
+apt-get update
+apt-get install -y yandex-browser-stable

9f30c70b32f72155f775f352f22be291c5a719ba
degex26 <workmolko@gmail.com>
2026-06-18 21:31:00 +0700

    Update 1_hq-srv.sh

--- 1_hq-srv.sh
+++ 1_hq-srv.sh
@@ -7,11 +7,11 @@ timedatectl set-timezone Asia/Novosibirsk
 hostnamectl set-hostname hq-srv.au-team.irpo
 
 echo 'BOOTPROTO=static' > /etc/net/ifaces/ens18/options
-echo "192.168.1.2/27" > /etc/net/ifaces/ens18/ipv4address
-echo "default via 192.168.1.1" > /etc/net/ifaces/ens18/ipv4route
+echo "192.168.113.2/27" > /etc/net/ifaces/ens18/ipv4address
+echo "default via 192.168.113.1" > /etc/net/ifaces/ens18/ipv4route
 systemctl restart network
 
-useradd -s /bin/bash -u 2026 sshuser
+useradd -s /bin/bash -u 2012 sshuser
 echo "sshuser:P@ssw0rd" | chpasswd
 gpasswd -a sshuser wheel
 echo 'sshuser ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
@@ -21,5 +21,5 @@ echo "AllowUsers sshuser" >> /etc/openssh/sshd_config
 echo "Authorized access only" > /etc/openssh/banner
 systemctl restart sshd
 
-echo "server 172.16.1.1 iburst" > /etc/chrony.conf
+echo "server 172.16.30.1 iburst" > /etc/chrony.conf
 systemctl restart chronyd

0e369e66c8d3f15daa2eaf8189b84011bff0d6a9
degex26 <workmolko@gmail.com>
2026-06-18 21:35:20 +0700

    Update 2_br-rtr.sh

--- 2_br-rtr.sh
+++ 2_br-rtr.sh
@@ -1,5 +1,11 @@
 enable
 configure terminal
-ip nat source static tcp 192.168.2.2 8080 172.16.2.2 8080
-ip nat source static tcp 192.168.2.2 2026 172.16.2.2 2026
+
+security-profile 0
+ rule 0 permit tcp any any eq 22
+ security 0
+
+ip nat source static tcp 192.168.2.2 8080 172.16.40.2 8080
+ip nat source static tcp 192.168.2.2 2026 172.16.40.2 2026
+
 write memory

987bce4f9955b7d0fd93c5eba624cedafa0d2e39
degex26 <workmolko@gmail.com>
2026-06-18 21:34:49 +0700

    Update 2_hq-rtr.sh

--- 2_hq-rtr.sh
+++ 2_hq-rtr.sh
@@ -5,7 +5,7 @@ security-profile 0
  rule 0 permit tcp any any eq 22
  security 0
 
-ip nat source static tcp 192.168.1.2 80 172.16.30.2 8080
-ip nat source static tcp 192.168.1.2 2026 172.16.30.2 2026
+ip nat source static tcp 192.168.113.2 80 172.16.30.2 8080
+ip nat source static tcp 192.168.113.2 2026 172.16.30.2 2026
 
 write memory
